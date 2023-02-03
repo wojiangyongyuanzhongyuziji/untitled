575efa4e88a04cbb172ea1be4a33732bf133006b
“qinguoxing” <925632234@qq.com>
2023-02-03 17:30:02 +0800

    first commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,7 +5,7 @@
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println(123123);
+        System.out.println("第一个版本");
+
     }
 }
